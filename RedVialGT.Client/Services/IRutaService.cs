01b3a81f592d9810dc58df15783ad46507f32980
WilmerS95 <wilmerantoniosinay@gmail.com>
2024-04-23 13:08:28 -0600

    Interface ruta

---
+++ RedVialGT.Client/Services/IRutaService.cs
@@ -0,0 +1,14 @@
+﻿using RedVialGT.Shared;
+
+
+namespace RedVialGT.Client.Services
+{
+    public interface IRutaService
+    {
+        Task<List<RutaDTO>> Lista();
+        Task<RutaDTO> Buscar(int id);
+        Task<int> Guardar(RutaDTO ruta);
+        Task<int> Editar(RutaDTO ruta);
+        Task<bool> Eliminar(int id);
+    }
+}
