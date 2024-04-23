de63084d85efd6bdf4fe24ab45fa52c4cf22001e
WilmerS95 <wilmerantoniosinay@gmail.com>
2024-04-23 13:50:27 -0600

    Implementation Services

--- RedVialGT.Client/Program.cs
+++ RedVialGT.Client/Program.cs
@@ -1,11 +1,15 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using RedVialGT.Client;
+using RedVialGT.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5150") });
+
+builder.Services.AddScoped<IDepartamentoService, DepartamentoService>();
+builder.Services.AddScoped<IRutaService, RutaService>();
 
 await builder.Build().RunAsync();
