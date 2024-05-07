fe4c05a34e43e742becb68f55ae85b854eb15155
WilmerS95 <wilmerantoniosinay@gmail.com>
2024-05-07 03:37:41 -0600

    Nodo add

---
+++ RedVialGT.Client/DTO/Nodo.cs
@@ -0,0 +1,22 @@
+﻿using RedVialGT.Shared;
+
+namespace RedVialGT.Client.DTO
+{
+    public class Nodo
+    {
+        public Nodo? ligaOeste { get; set; }
+        public Nodo? ligaEste { get; set;}
+        public Nodo? ligaSur {  get; set; }
+        public Nodo? ligaNorte { get; set; }
+        public RutaDTO ruta { get; set; }
+
+        public Nodo(RutaDTO ruta)
+        {
+            this.ligaOeste = ligaOeste;
+            this.ligaEste = ligaEste;
+            this.ligaSur = ligaSur;
+            this.ligaNorte = ligaNorte;
+            this.ruta = ruta;
+        }
+    }
+}
